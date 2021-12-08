5fdb02c1d84659d99eb8da32632eae483cf1129e
SaheenJasmine <63956055+developweb-Sah@users.noreply.github.com>
2021-12-08 21:32:19 +0530

    Change in Bar graph and direct fetching of data from api

--- src/components/FeaturedNews/FeaturedNews.jsx
+++ src/components/FeaturedNews/FeaturedNews.jsx
@@ -1,104 +1,101 @@
-import React from 'react';
-import './FeaturedNews.css';
+import React from "react";
+import "./FeaturedNews.css";
 import {
   Button,
   Card,
   CardActions,
   CardContent,
   Typography,
-} from '@material-ui/core';
+} from "@material-ui/core";
 
 export default function FeaturedNews({ positive, negative }) {
   return (
-    <div className='featured'>
-      <div className='featuredItem'>
+    <div className="featured">
+      <div className="featuredItem">
         <Card
           style={{
             width: 300,
-            backgroundColor: '#5ba5b0',
+            backgroundColor: "#5ba5b0",
           }}
         >
           <CardContent>
             <Typography
               style={{ fontSize: 14 }}
-              color='textSecondary'
+              color="textSecondary"
               gutterBottom
             ></Typography>
-            <Typography variant='h5' component='h2' style={{ color: 'white' }}>
+            <Typography variant="h5" component="h2" style={{ color: "white" }}>
               Total
             </Typography>
             <Typography
               style={{
                 marginBottom: 12,
-                color: 'white',
-                marginLeft: '1px',
+                color: "white",
+                marginLeft: "1px",
               }}
             >
               {positive + negative}
             </Typography>
-            <Typography variant='body2' component='p'></Typography>
+            <Typography variant="body2" component="p"></Typography>
           </CardContent>
-         
         </Card>
       </div>
-      <div className='featuredItem'>
+      <div className="featuredItem">
         <Card
           style={{
             width: 300,
-            backgroundColor: '#e85b51',
+            backgroundColor: "#e85b51",
           }}
         >
           <CardContent>
             <Typography
               style={{ fontSize: 14 }}
-              color='textSecondary'
+              color="textSecondary"
               gutterBottom
             ></Typography>
-            <Typography variant='h5' component='h2' style={{ color: 'white' }}>
+            <Typography variant="h5" component="h2" style={{ color: "white" }}>
               Negative
             </Typography>
             <Typography
               style={{
                 marginBottom: 12,
-                color: 'white',
-                marginLeft: '1px',
+                color: "white",
+                marginLeft: "1px",
               }}
             >
               {negative}
             </Typography>
-            <Typography variant='body2' component='p'></Typography>
+            <Typography variant="body2" component="p"></Typography>
           </CardContent>
-         
         </Card>
       </div>
-      <div className='featuredItem'>
+      <div className="featuredItem">
         <Card
           style={{
             width: 300,
-            backgroundColor: '#38a662',
+            backgroundColor: "#38a662",
           }}
         >
           <CardContent>
             <Typography
               style={{ fontSize: 14 }}
-              color='textSecondary'
+              color="textSecondary"
               gutterBottom
             ></Typography>
-            <Typography variant='h5' component='h2' style={{ color: 'white' }}>
+            <Typography variant="h5" component="h2" style={{ color: "white" }}>
               Positive
             </Typography>
             <Typography
               style={{
                 marginBottom: 12,
-                color: 'white',
-                marginLeft: '1px',
+                color: "white",
+                marginLeft: "1px",
               }}
             >
               {positive}
             </Typography>
-            <Typography variant='body2' component='p'></Typography>
+            <Typography variant="body2" component="p"></Typography>
           </CardContent>
-        
         </Card>
       </div>
     </div>
